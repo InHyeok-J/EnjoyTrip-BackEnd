4e54a9ee56fbbc7fce3ba281c00a96660d77d738
inhyeok <ss112d86@gmail.com>
2023-05-03 17:28:51 +0900

    Feat: User 정보 조회 기능 구현

--- src/main/java/com/enjoytrip/user/controller/UserController.java
+++ src/main/java/com/enjoytrip/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.enjoytrip.user.controller;
 
 
+import com.enjoytrip.global.dto.SessionUser;
+import com.enjoytrip.global.exception.BusinessException;
 import com.enjoytrip.global.response.JsonResponse;
 import com.enjoytrip.user.controller.dto.SignUpRequest;
 import com.enjoytrip.user.service.UserService;
@@ -8,6 +10,8 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +27,13 @@ public class UserController {
         userService.signUp(requestDto);
         return JsonResponse.ok(HttpStatus.CREATED, "회원가입 성공");
     }
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getLoginUserInfo(@AuthenticationPrincipal SessionUser sessionUser){
+        if(sessionUser == null){
+            throw new BusinessException("로그인이 필요합니다",401);
+        }
+        return JsonResponse.okWithData(HttpStatus.OK,"유저 정보 조회성공",sessionUser);
+    }
+
 }
