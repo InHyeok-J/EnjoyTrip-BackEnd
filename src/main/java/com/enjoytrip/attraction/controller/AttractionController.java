f30d6c3023984a41aa3d151247e26011216985f5
synodical <lena0323park@naver.com>
2023-05-03 13:59:01 +0900

    Refactor : 불필요한 Import 삭제

--- src/main/java/com/enjoytrip/attraction/controller/AttractionController.java
+++ src/main/java/com/enjoytrip/attraction/controller/AttractionController.java
@@ -7,9 +7,7 @@ import com.enjoytrip.global.response.JsonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
