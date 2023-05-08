936635e10967c9890fbe56b0991e8bda89b4fb2e
inhyeok <ss112d86@gmail.com>
2023-05-08 13:22:26 +0900

    Feat: Mail 전송 기능 구현

--- src/main/java/com/enjoytrip/auth/service/AuthEmailSender.java
+++ src/main/java/com/enjoytrip/auth/service/AuthEmailSender.java
@@ -1,17 +1,35 @@
 package com.enjoytrip.auth.service;
 
 
+import com.enjoytrip.global.exception.BusinessException;
+import javax.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 
 @Slf4j
 @Component
-public class AuthEmailSender implements AuthEmailService{
+@RequiredArgsConstructor
+public class AuthEmailSender implements AuthEmailService {
 
+    private final JavaMailSender javaMailSender;
 
     @Override
     public void sendEmail(String code, String email) {
-        log.info("send Email = code => " + code + " EMAIL = > " + email);
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false,
+                "UTF-8");
+            mimeMessageHelper.setTo(email);
+            mimeMessageHelper.setSubject("[EnjoyTrip] 이메일 인증 코드입니다.");
+            mimeMessageHelper.setText("인증 코드 : "+code);
+            javaMailSender.send(mimeMessage);
+        } catch (Exception e) {
+            log.warn("이메일 전송 실패");
+            throw new BusinessException("이메일 전송 실패!!", 500);
+        }
     }
 }
