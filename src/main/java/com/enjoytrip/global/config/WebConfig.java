e4272ef50e228f1385f986cf2c1fc77f84f0799a
synodical <lena0323park@naver.com>
2023-05-21 17:50:02 +0900

    Fix : 프엔 port number to 3000

--- src/main/java/com/enjoytrip/global/config/WebConfig.java
+++ src/main/java/com/enjoytrip/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080")
+            .allowedOrigins("http://localhost:3000")
             .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
             .allowCredentials(true);
     }
