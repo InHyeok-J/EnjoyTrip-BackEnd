b931f54ad1bd74143921962f286372d8b86e4862
synodical <lena0323park@naver.com>
2023-05-23 22:32:39 +0900

    Feat : 구군 코드 불러오는 api

--- src/main/java/com/enjoytrip/attraction/service/AttractionService.java
+++ src/main/java/com/enjoytrip/attraction/service/AttractionService.java
@@ -1,6 +1,7 @@
 package com.enjoytrip.attraction.service;
 
 import com.enjoytrip.attraction.dto.AttractionDetailResDto;
+import com.enjoytrip.attraction.dto.AttractionGugunDto;
 import com.enjoytrip.attraction.dto.AttractionListResDto;
 import com.enjoytrip.attraction.dto.AttractionSearchOptionsDto;
 import com.enjoytrip.attraction.entity.Attraction;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface AttractionService {
     List<AttractionListResDto> search(AttractionSearchOptionsDto searchOptions);
     AttractionDetailResDto getDetail(String attractionId);
+    List<AttractionGugunDto> getGugun(String sidoCode);
 }
